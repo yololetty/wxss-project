f3104fea52359dc556548479e62704c419c7e375
cxf <cxfyolo@gmail.com>
2020-03-07 21:55:56 +0800

    this is wxxcx login demo

---
+++ wx-login/pages/login/login.js
@@ -0,0 +1,82 @@
+var app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    username: null,
+    passward: null
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+  usernameInput: function(e){
+    this.setData({username: e.detail.value})
+  },
+  passwardInput: function (e) {
+    this.setData({passward: e.detail.value})
+  },
+  clickLog: function(){
+    app.appData.userInfo = {
+      username: this.data.username,
+      passward: this.data.passward
+    };
+    wx.switchTab({
+      url: '../user/user',
+    })
+  }
+})
